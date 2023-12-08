cf5090c8386d3b94be48fb8d0ef8614c4fa077f1
Zindriana <hulten.johan22@gmail.com>
2023-12-08 16:00:41 +0100

    skapat ett startfönster för programmet

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 
     public static void main(String[] args){
+        launch(args);
+    }
 
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Awesome Hangman v0.1");
+
+        Button btn = new Button();
+        btn.setText("Start a new game");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
