6e45f345a02a30de149a0496ecc668a8886c9fef
Nasser Amer <nasser_amer09@yahoo.com>
2023-12-11 13:51:48 +0100

    Made a player-list class to save user information

---
+++ src/PlayerList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PlayerList {
+    //A list to keep player items
+    private List<Player> players;
+
+
+    public PlayerList(){
+        this.players=new ArrayList<>();
+    }
+    //To avoid direct modification it calls a copy of the player list
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+// To adding one player at a time
+    public void addPlayer(Player player){
+        players.add(player);
+    }
+
+    // SHOW PLAYER INFORMATION ONE BY ONE, IT WOULD BE GOOD IF WE CREATE A SEPARATE CLASS FOR THIS PURPOSE
+    public void showPlayers(){
+        for (Player player: players
+             ) {
+            System.out.println("Player name [" + player.getName() + "]\nGuessed Word " + "[" + player.getGuessedWord() + "]");
+        }
+    }
+//WORDS TO USE IN VERSION 1
+    public static List<String> wordToGuess(){
+        return Arrays.asList("SNÖ", "JUL", "LAX", "GRAN", "GÅVA", "STJÄRNA", "LJUS", "TOMTE");
+    }
+
+
+}
