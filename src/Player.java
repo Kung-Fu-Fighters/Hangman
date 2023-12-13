73fab49003f78707ce1a14cc5099023bce158b73
Nasser Amer <nasser_amer09@yahoo.com>
2023-12-13 10:43:01 +0100

    Deleted playerInfo methode and replaced it with som parameters in the construktor

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Player {
     private String name;
     private String guessedWord;
@@ -7,45 +5,27 @@ public class Player {
     private int scorePoints;
 
     // När klassen anropas kommer användaren tillfrågas om info
-    public Player(){
-       playerInfo();
+    public Player(String name, String guessedWord){
+     this.name=name;
+     this.guessedWord=guessedWord;
     }
-    private void playerInfo(){
-        Scanner scanner = new Scanner(System.in);
-        //kontroll så att spelaren inte fortsätter utan att hen har skrivit sitt namn
-        do {
-            System.out.println("Skriv det namnet du vill använda när du spelar här:  ");
-            this.name=scanner.nextLine();
-        }while (name.isEmpty());
-
-        do {
-            System.out.println("Skriv in ett ord som andra spelare ska gissa på här:  ");
-            this.guessedWord=scanner.nextLine();
-        }while (guessedWord.isEmpty());
-
-    }
-    public String getName() {
+    public String getName(){
         return name;
     }
-
-    public String getGuessedWord() {
+    public String getGuessedWord(){
         return guessedWord;
     }
 
-    public void setGuessedWord(String guessedWord) {
+    public void setGuessedWord(String guessedWord){
         this.guessedWord = guessedWord;
     }
 
-    public int getScorePoints() {
+    public int getScorePoints(){
         return scorePoints;
     }
 
-    public void setScorePoints(int scorePoints) {
+    public void setScorePoints(int scorePoints){
         this.scorePoints = scorePoints;
     }
-
-
-
-
 
 }
